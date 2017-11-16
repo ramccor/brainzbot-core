310d55098e86ebb1ffc29d6bcb621951b9ab1940
LeoVerto <LeoVerto@users.noreply.github.com>
2017-11-16 02:41:39 +0100

    Add "braindump" cmd & unicode support to metabrain plugin

--- botbot_plugins/plugins/metabrain.py
+++ botbot_plugins/plugins/metabrain.py
@@ -1,5 +1,7 @@
+import json
+
 from ..base import BasePlugin
-from ..decorators import listens_to_regex_command
+from ..decorators import listens_to_command, listens_to_regex_command
 
 
 class Plugin(BasePlugin):
@@ -21,13 +23,26 @@ class Plugin(BasePlugin):
 
         stuff I need to remember
 
-    To make me forget something user
+    To make me forget something use
 
         {{ command_prefix }}forget thing
+
+    To display all the things I've remembered use
+
+        {{ command_prefix }}braindump
     """
 
     @listens_to_regex_command("remember", ur"(?P<key>.+?)=\s*(?P<value>.*)")
     def remember(self, line, key, value):
+        try:
+            memory = json.loads(self.retrieve("memory"))
+        except TypeError:
+            memory = []
+
+        memory.append(key)
+
+        self.store("memory", json.dumps(memory))
+
         self.store(key, value)
         return u'I will remember "{0}" for you {1}.'.format(key, line.user)
 
@@ -37,9 +52,34 @@ class Plugin(BasePlugin):
         if value:
             return value
         else:
-            return u'I\'m sorry, I don\'t remember "{0}".'.format(key)
+            return u'I\'m sorry, I don\'t remember "{0}", are you sure I should know about it?'.format(key)
 
     @listens_to_regex_command("forget", ur"(?P<key>.*)")
     def forget(self, line, key):
+        try:
+            memory = json.loads(self.retrieve("memory"))
+        except TypeError:
+            memory = []
+
+        if key not in memory:
+            return u'I\'m sorry, I don\'t remember "{0}", are you sure I should know about it?'.format(key)
+
+        memory.remove(key)
+        self.store("memory", json.dumps(memory))
+
         self.delete(key)
         return u'What was "{0}" all about?'.format(key)
+
+    @listens_to_command("braindump")
+    def braindump(self, line, args):
+        try:
+            memory = json.loads(self.retrieve("memory"))
+        except TypeError:
+            memory = []
+
+        if len(memory) == 0:
+            return "I have no memory yet, use the recall command to make me remember stuff for you."
+        elif len(memory) == 1:
+            return u'I remember "{0}".'.format(memory[0])
+        else:
+            return u'I remember "{0}" and "{1}".'.format('", "'.join(memory[:-1]), memory[-1])
