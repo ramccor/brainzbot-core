36d01b25100e2faebb6b32c0ff0460d54d8d7601
Kartik Ohri <kartikohri13@gmail.com>
2025-07-14 21:46:37 +0530

    remove unused urlconf

--- botbot/apps/bots/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^manage/$', views.ManageChannel.as_view(), name='manage_channel'),
-    url(r'^delete/$', views.DeleteChannel.as_view(), name='delete_channel'),
-]
